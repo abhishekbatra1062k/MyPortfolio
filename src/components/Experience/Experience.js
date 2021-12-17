b1084f19880acc69e4da6ca57fa74b443fe357a0
abhishekbatra1062k <60574452+abhishekbatra1062k@users.noreply.github.com>
2021-12-18 04:09:07 +0530

    Update Experience.js

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,8 +2,8 @@ import './Experience.css'
 
 const Experience = ({ project }) => (
   <div className='project'>
-    <h4>{project.role}</h4> <br />
-    <h3>{project.companyName}</h3>
+    <h3>{project.companyName}</h3><br/>
+    <h4>{project.role}</h4>
     <p className='project__description'>{project.description}</p>
     <div className="img"><img src={project.img} alt={project.alt} /></div>
     <p className='project__description'>({project.duration})</p>
